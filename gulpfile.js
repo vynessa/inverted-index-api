2ab53f73a69a1bd7c012b45a8a2a051103c2d18f
vynessa <vanessa.ejikeme@gmail.com>
2017-04-29 03:39:48 +0100

    Modify gulf tasks to run tests, serve and handle coverage

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,10 @@
 const gulp = require('gulp'),
   gutil = require('gulp-util'),
   gulpNodemon = require('gulp-nodemon'),
+  expressServer = require('gulp-express'),
   jasmineNode = require('gulp-jasmine-node'),
-  istanbulReport = require('gulp-istanbul-report');
+  istanbulReport = require('gulp-istanbul-report'),
+  Server = require('karma').Server;
 
 const coverageFile = './coverage/coverage.json';
 const jasmineNodeOpts = {
@@ -18,28 +20,38 @@ gulp.task('default', () => {
   return gutil.log('gulp is running');
 });
 
-gulp.task('run-tests', () => {
-  return gulp.src(['./tests/**/*inverted-index-test.js']).pipe(jasmineNode(jasmineNodeOpts));
+// gulp.task('run-tests', () => {
+//   return gulp.src(['./tests/**/*inverted-index-test.js']).pipe(jasmineNode(jasmineNodeOpts));
+// });
+
+/**
+ * Run test once and exit
+ */
+gulp.task('run-tests', (done) => {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
 });
 
-// gulp.task('coverage', ['run-tests'], () => {
-//   gulp.src(coverageFile);
-//     .pipe(istanbulReport({
-//       reporters: [
-//         'text-summary',
-//         {
-//           name: 'json',
-//           file: 'cov.json',
-//           dir: './coverage'
-//         }
-//       ]
-//     }))
-// });
 
+gulp.task('serve', () => {
+  expressServer.run(['server.js']);
+});
 
-// gulp.task('coverage', () => {
-
-// });
+gulp.task('coverage', ['run-tests'], () => {
+  gulp.src(coverageFile)
+    .pipe(istanbulReport({
+      reporters: [
+        'text-summary',
+        {
+          name: 'json',
+          file: 'cov.json',
+          dir: './coverage'
+        }
+      ]
+    }));
+});
 
 // gulp.task('js', ['jscs', 'jshint'], () => {
 //   return gulp
