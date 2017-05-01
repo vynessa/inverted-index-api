c6101c8fecffca6250a73774fab2a25f0e363fce
vynessa <vanessa.ejikeme@gmail.com>
2017-05-01 22:29:41 +0100

    add new test files to fixtures

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express'),
+  app = express(),
+  dotenv = require('dotenv');
+
+// make express look in the public directory
+dotenv.config();
+
+const port = process.env.PORT_TEST;
+
+app.use(express.static(__dirname + '/src'))
+app.get('/', (res, req) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(`Magic happens on port http://localhost:' + ${port}`);
+});
