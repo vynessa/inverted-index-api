19bf310ddaa6ab795dba72a5c9fc92e39a348f67
vynessa <vanessa.ejikeme@gmail.com>
2017-05-02 11:49:03 +0100

    update invrted index test file validity method

--- tests/inverted-index-test.js
+++ tests/inverted-index-test.js
@@ -1,49 +1,62 @@
-// 'use strict'
-// var  = require('../src/library.js').;
+const invertedIndex = require('../src/inverted-index.js').InvertedIndex();
+// const invertedIndex = new InvertedIndex();
+const validFile = [
+  {
+    "title": "dsdsdsd",
+    "text": "dsdsdsd"
+  },
+  {
+    "title": "sdsdsds",
+    "text": "sdsdsd"
+  }
+];
 
-describe('Get array of prime: ', () => {
-  describe('Verify that the array of primes is accurate', () => {
-    it('should return [2, 3, 5, 7] for 7', () => {
-      expect(7).toEqual([2, 3, 5, 7]);
-    });
-    it('should return [2, 3, 5, 7, 11, 13, 17] for 17', () => {
-      expect(17).toEqual([2, 3, 5, 7, 11, 13, 17]);
-    });
-    it('should return [2, 3] for 3', () => {
-      expect(3).toEqual([2, 3]);
-    });
-    it('should return [2, 3, 5, 7] for 10', () => {
-      expect(10).toEqual([2, 3, 5, 7]);
-    });
-    it('should return [2, 3, 5] for 6', () => {
-      expect(6).toEqual([2, 3, 5]);
-    });
-  });
+const malformed = [
+  {
+    "ti": "dsdsdsd",
+    "tet": "dsdsdsd"
+  },
+  {
+    "ti": "sdsdsds",
+    "tt": "sdsdsd"
+  }
+];
 
-  describe('Test should check for valid inputs', () => {
-    it('should return \'Invalid input\' for numbers less than 2', () => {
-      expect(1).toEqual('Invalid input');
+const invalid = ['cold'];
+
+const empty = [
+  {
+    "title": "",
+    "text": ""
+  },
+  {
+    "title": "",
+    "text": ""
+  }
+];
+
+describe('Inverted Index Suite', () => {
+  describe('The inverted index isValid method', () => {
+    const validBook = invertedIndex.isValid(book);
+
+    it('should return type of object', () => {
+      expect(validBook instanceof Object).toBeTruthy();
     });
-    it('should return \'Invalid input\' for numbers less than 2', () => {
-      expect(-1).toEqual('Invalid input');
+
+    it('should return valid JSON for valid books', () => {
+      expect(validBook(validFile)).toEqual('Valid JSON');
     });
-    it('should return \'Invalid input\' for non-integers', () => {
-      expect('a').toEqual('Invalid input');
+
+    it('should return Invalid JSON for invalid books', () => {
+      expect(validBook(invalid)).toEqual('Valid JSON');
     });
-    it('should return \'Invalid input\' for non-integers', () => {
-      expect('abc').toEqual('Invalid input');
+
+    it('should return Malformed JSON for Malformed books', () => {
+      expect(validBook(malformed)).toEqual('Malformed JSON');
     });
-    it('should return \'Invalid input\' for non-integers', () => {
-      expect(-9).toEqual('Invalid input');
-    });
-    it('should return \'Invalid input\' for non-integers', () => {
-      expect('day').toEqual('Invalid input');
-    });
-    it('should return \'Invalid input\' for non-integers', () => {
-      expect('-').toEqual('Invalid input');
-    });
-    it('should return \'Invalid input\' for non-integers', () => {
-      expect('/').toEqual('Invalid input');
+
+    it('should return Empty JSON for Empty books', () => {
+      expect(validBook(empty)).toEqual('Empty JSON');
     });
   });
 });
