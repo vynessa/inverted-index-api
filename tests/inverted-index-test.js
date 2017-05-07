40114a2d40e43ae534b7ded691b4d6f2e0eeb048
vynessa <vanessa.ejikeme@gmail.com>
2017-05-07 17:31:35 +0100

    Add and modify tests for tokenize, joinTextTitle and removeDuplicates method

--- tests/inverted-index-test.js
+++ tests/inverted-index-test.js
@@ -8,16 +8,18 @@ import InvertedIndex from '../src/inverted-index';
 
 const invObj = new InvertedIndex();
 
-
-
 describe('Inverted Index Suite:', () => {
+  // describe('Read File function', () => {
+  //   it('should', () => {
+
+  //   });
   const validBook = invObj.isValid(validFile);
   const invalidBook = invObj.isValid(invalidFile);
   const malformedBook = invObj.isValid(malformedFile);
   const emptyBook = invObj.isValid(emptyFile);
 
   describe('The inverted index isValid method', () => {
-    it('should return type of object', () => {
+    it('should return type of ', () => {
       expect(validBook).toEqual('Valid JSON');
     });
 
@@ -34,61 +36,123 @@ describe('Inverted Index Suite:', () => {
     });
   });
 
-  describe('Tokenize function', () => {
-    it('should return in an array tokenized words for each file', () => {
-      // let book1 = [
-      //   {
-      //     'title': 'Alice in the wonder land',
-      //     'text': 'I am not as lazy as vanilla'
-      //   },
-      //   {
-      //     'title': 'Girl',
-      //     'text': 'SHe has a child'
-      //   }
-      // ];
+  describe('Join text and title function', () => {
+    it('should return a string with the joined text and title in lowercases', () => {
+      let newBook = [
+        {
+          'title': 'Alice in the wonder land',
+          'text': 'I am not as lazy as Alice'
+        },
+        {
+          'title': 'Lady',
+          'text': 'The lady is 50 years old. SHe has a child'
+        }
+      ];
 
-      let result =
-        ['a',
-          'alice',
-          'am',
-          'as',
-          'as',
-          'child',
-          'girl',
-          'has',
-          'i',
-          'in',
-          'land',
-          'lazy',
-          'not',
-          'she',
-          'the',
-          'vanilla',
-          'wonder'];      
-
-      // book1 = invObj.tokenize(book1);
-
-      expect(invObj.tokenize(validBook)).toEqual(result);
+      const newTextTitle = 'alice in the wonder land i am not as lazy as alice lady the lady is 50 years old. she has a child'
+      newBook = InvertedIndex.joinTextTitle(newBook);
+      expect(newTextTitle).toEqual(newBook);
     });
   });
 
-  // describe('Remove duplicates word function', () => {
+  describe('Remove duplicates word', () => {
+    it('should return only unique words in an array', () => {
+      let getWords = 
+      [ 'broken',
+        'colour',
+        'crayons',
+        'go',
+        'go',
+        'he',
+        'he',
+        'is',
+        'is',
+        'poem',
+        'premier',
+        'queen',
+        'still',
+        'to',
+        'to',
+        'up',
+        'up',
+        'yes' ];
 
-  // });
+      const uniqueWords = 
+      [ 'broken',
+        'colour',
+        'crayons',
+        'go',
+        'he',
+        'is',
+        'poem',
+        'premier',
+        'queen',
+        'still',
+        'to',
+        'up',
+        'yes' ]
 
+      getWords = InvertedIndex.removeDuplicates(getWords);
+      expect(uniqueWords).toEqual(getWords);
+    });
+  });
+
+  describe('Tokenize function', () => {
+    it('should return in an array tokens for each file', () => {
+      let newBook = [
+        {
+          'title': 'Alice in the wonder land',
+          'text': 'I am not as lazy as Alice'
+        },
+        {
+          'title': 'Lady',
+          'text': 'The lady is 50 years old. SHe has a child'
+        }
+      ];
+
+      const result =
+      [ 'a',
+        'alice',
+        'am',
+        'as',
+        'as',
+        'child',
+        'girl',
+        'has',
+        'i',
+        'in',
+        'land',
+        'lazy',
+        'not',
+        'she',
+        'the',
+        'vanilla',
+        'wonder'];      
+
+      newBook = invObj.tokenize(newBook);
+      expect(result).toEqual(newBook);
+    });
+  });
+
+
+  
   // describe('Create Index function', () => {
-
-  // });
-
-  // describe('Read File function', () => {
-  //   it('should', () => {
-
+  //   it('should return the accurate index for each word\'s  occurence in a book', () => {
+  //     let 
+  //     const createdIndex = 
+  //     { go: [ 0 ],
+  //       he: [ 0, 1 ],
+  //       is: [ 0 ],
+  //       premier: [ 0 ],
+  //       queen: [ 0 ],
+  //       to: [ 0 ],
+  //       up: [ 0 ],
+  //       yes: [ 0 ],
+  //       broken: [ 1 ],
+  //       colour: [ 1 ],
+  //       crayons: [ 1 ],
+  //       poem: [ 1 ],
+  //       still: [ 1 ] }
   //   });
-
-  //   it('should', () => {
-
-  //   });
   // });
-
-
 });
