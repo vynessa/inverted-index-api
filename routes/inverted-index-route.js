d59d357ca947bc102c24a9d3e1735db2638516ec
vynessa <vanessa.ejikeme@gmail.com>
2017-05-09 20:15:17 +0100

    Implement create and search routes using express

---
+++ routes/inverted-index-route.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import InvertedIndex from '../src/inverted-index.js';
+
+const router = express.Router();
+// const validFile = 
+const invController = new InvertedIndex();
+
+router.post('/api/create', (req, res) => {
+  invController.createIndex('ValidFile', req.body);
+  res.json(invController.allIndices);
+});
+
+
+router.post('/api/search', (req, res) => {
+  const index = invController.allIndices;
+  const filename = req.body.fileName;
+  const searchTerms = req.body.searchTerms;
+
+  const searchResult = invController.searchIndex(index, filename, searchTerms)
+  res.json(searchResult);
+});
+
+
+module.exports = router;
