eeb65439c2c0f549107126fd050f4321e84c3451
vynessa <vanessa.ejikeme@gmail.com>
2017-05-10 06:52:32 +0100

    Add and clean up comments

--- routes/inverted-index-route.js
+++ routes/inverted-index-route.js
@@ -1,16 +1,17 @@
 import express from 'express';
 import InvertedIndex from '../src/inverted-index.js';
 
+// Call an instance of Router from express
 const router = express.Router();
-// const validFile = 
 const invController = new InvertedIndex();
 
+// Create route  
 router.post('/api/create', (req, res) => {
   invController.createIndex('ValidFile', req.body);
   res.json(invController.allIndices);
 });
 
-
+// Search route 
 router.post('/api/search', (req, res) => {
   const index = invController.allIndices;
   const filename = req.body.fileName;
