a65aa38a26d44cb26730400c2280786e7045f2f4
vynessa <vanessa.ejikeme@gmail.com>
2017-05-10 06:53:04 +0100

    Add comments

--- server.js
+++ server.js
@@ -2,27 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 
+// Call an instance of express and store it in the app variable
 const app = express();
-
-// Import InvertedIndex class
-// const 
-
 
 // make express look in the public directory
 dotenv.config({ path: '.env' });
 
+// (TODO): Change port to avoid blinding the env variable on heroku
 const port = process.env.PORT_DEV;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static(`${__dirname}/src`));
 app.use(require('./routes/inverted-index-route.js'));
-
-// app.get('/', (res, req) => {
-//   res.render('index');
-// });
-
-// http.createServer();
 
 app.listen(port, () => {
   console.log(`Magic happens on port http://localhost:${port}`);
