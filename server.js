1ee2f59535175274f5b66146cb692dc973816a43
vynessa <vanessa.ejikeme@gmail.com>
2017-05-12 11:16:44 +0100

    Clean up unnecessary comments and modify imports

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import routes from './routes/inverted-index-route.js';
 
 // Call an instance of express and store it in the app variable
 const app = express();
@@ -13,10 +14,10 @@ const port = process.env.PORT_DEV;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static(`${__dirname}/src`));
-app.use(require('./routes/inverted-index-route.js'));
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Magic happens on port http://localhost:${port}`);
 });
 
-module.exports = app;
+export default app;
