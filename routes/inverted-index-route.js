b5138aedeaf1c7a5b7d5f2eb66ab028d73559b6a
vynessa <vanessa.ejikeme@gmail.com>
2017-05-12 11:15:45 +0100

    Implement Multer for file uploads on postman

--- routes/inverted-index-route.js
+++ routes/inverted-index-route.js
@@ -1,14 +1,29 @@
 import express from 'express';
-import InvertedIndex from '../src/inverted-index';
+import multer from 'multer';
+// import bodyParser from 'body-parser';
+import InvertedIndex from '../src/inverted-index.js';
 
 // Call an instance of Router from express
 const router = express.Router();
 const invController = new InvertedIndex();
 
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, './uploads');
+  },
+  filename: (req, file, callback) => {
+    callback(null, `${file.originalname}`);
+  }
+});
+
+const upload = multer({ storage: storage });
+
 // Create route
-router.post('/api/create', (req, res) => {
-  invController.createIndex('ValidFile', req.body);
-  res.json(invController.allIndices);
+router.post('/api/create', upload.array('files'), (req, res) => {
+  const files = req.files;
+  const fileSpec = invController.getMulterJson(files);
+  const index = invController.createIndex(fileSpec);
+  res.status(200).json(invController.allIndices);
 });
 
 // Search route
@@ -16,10 +31,8 @@ router.post('/api/search', (req, res) => {
   const index = invController.allIndices;
   const filename = req.body.fileName;
   const searchTerms = req.body.searchTerms;
-
   const searchResult = invController.searchIndex(index, filename, ...searchTerms);
-  res.json(searchResult);
+  res.status(200).json(searchResult);
 });
 
-
-module.exports = router;
+export default router;
