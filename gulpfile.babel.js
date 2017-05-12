d078828ceb1bdd55666aeef266db93aa0663d99b
vynessa <vanessa.ejikeme@gmail.com>
2017-05-12 11:17:40 +0100

    Fix import issue causing parsing error on InvertedIndex clas

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,61 +1,65 @@
 import gulp from 'gulp';
 import gulpNodemon from 'gulp-nodemon';
-import expressServer from 'gulp-express';
 import jasmineNode from 'gulp-jasmine-node';
-import istanbul from 'gulp-istanbul';
-import istanbulReport from 'gulp-istanbul-report';
+import istanbul from 'gulp-babel-istanbul';
 import gulpCoveralls from 'gulp-coveralls';
 import babel from 'gulp-babel';
-import babelRegister from 'babel-register';
+import injectModules from 'gulp-inject-modules';
 
-  // Server = require('karma').Server;
-
-// const coverageFile = './coverage/coverage.json';
 const jasmineNodeOpts = {
   timeout: 1000,
   includeStackTrace: false,
   color: true
 };
 
+gulp.task('api-tests', () => {
+  gulp.src('./tests/api-test.js')
+    .pipe(babel())
+    .pipe(jasmineNode(jasmineNodeOpts));
+});
+
 gulp.task('run-tests', () => {
-  gulp.src(['tests/*.js']).pipe(jasmineNode(jasmineNodeOpts));
+  return gulp.src(['tests/*.js'])
+    .pipe(jasmineNode(jasmineNodeOpts));
 });
 
 gulp.task('serve', () => {
   gulpNodemon({
     script: 'index.js',
     ext: 'js html',
-    env: { 'NODE_ENV': process.env.NODE_ENV }
+    env: { NODE_ENV: process.env.NODE_ENV }
   });
-  // gulp.src('index.js')
-  //   .pipe(babel())
-  //   .pipe(gulpNodemon());
-  // // expressServer.run('index.js');
 });
 
-gulp.task('pre-test', () => {
-  gulp.src(['src/inverted-index.js', './server.js'])
+gulp.task('coverage', (cb) => {
+  gulp.src('src/inverted-index.js')
     .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
+    .pipe(istanbul.hookRequire())
+    .on('finish', () => {
+      gulp.src('tests/inverted-index-test.js')
+      .pipe(babel())
+      .pipe(injectModules())
+      .pipe(jasmineNode())
+      .pipe(istanbul.writeReports())
+      .pipe(istanbul.enforceThresholds({ thresholds: { global: 30 } }))
+      .on('end', cb);
+    });
 });
 
-gulp.task('test', ['pre-test'], () => {
-  gulp.src(['tests/inverted-index-test.js'])
-    .pipe(jasmineNode(jasmineNodeOpts))
-    .pipe(istanbul.writeReports());
+gulp.task('test', () => {
+  gulp.src('tests/inverted-index-test.js')
+  .pipe(babel())
+  .pipe(jasmineNode(jasmineNodeOpts));
 });
 
-gulp.task('coverage', ['test'], () => {
-  gulp.src('test/coverage/**/lcov.info')
+// Load code coverage to coveralls
+gulp.task('coveralls', () => {
+  // If not running on CI environment it won't send lcov.info to coveralls
+  if (!process.env.CI) {
+    return;
+  }
+  return gulp.src('/lcov.info')
     .pipe(gulpCoveralls());
 });
 
-
-// gulp.task('js', ['jscs', 'jshint'], () => {
-//   return gulp
-//     .src('./src/**/*.js')
-//     .pipe(conat('all.js'))
-//     .pipe(uglify())
-//     .pipe(gulp.dest('./build/'));
-// });
 
