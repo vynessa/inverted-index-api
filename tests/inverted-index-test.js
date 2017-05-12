2cf7fd7de7ee06541c0a6ce43c073f9c3c3e6e1f
vynessa <vanessa.ejikeme@gmail.com>
2017-05-12 11:14:06 +0100

    Modify tests cases and factor in validateFile method

--- tests/inverted-index-test.js
+++ tests/inverted-index-test.js
@@ -1,8 +1,3 @@
-/* eslint no-undef: 0 */
-// import fs from 'fs';
-import supertest from 'supertest';
-import express from 'express';
-
 import validFile from '../fixtures/validFile.json';
 import invalidFile from '../fixtures/invalidFile.json';
 import emptyFile from '../fixtures/emptyFile.json';
@@ -11,40 +6,33 @@ import InvertedIndex from '../src/inverted-index';
 
 // Create an instance of InvertedIndex class
 const invObj = new InvertedIndex();
-// const app = express();
-// const request = supertest(app);
+const fileContent = invObj.validateFile('validFile.json');
+const invalidContent = invObj.validateFile('hello.txt');
 
-// Test Parameter for createIndex test
-const createdIndex =
-  { validFile:
-  { go: [0],
-    he: [0],
-    is: [0],
-    premier: [0],
-    queen: [0],
-    to: [0],
-    up: [0],
-    yes: [0],
-    broken: [1],
-    colour: [1],
-    crayons: [1],
-    poem: [1],
-    still: [1] } };
+// Testspec with fileSpec parameters passed in
+const validTestSpec = [{
+  filename: 'validFile.json',
+  fileContent: [{
+    'title': 'Queen Premier',
+    'text': 'Is he to go up? Yes, he is to go up.'
+  },
+  {
+    'title': 'Poem',
+    'text': 'Broken crayons still colour.'
+  }]
+}];
 
-const searchResult =
-  { validFile:
-  { crayons: [1],
-    poem: [1],
-    colour: [1] } };
+const invalidTestSpec = [{
+  filename: 'invalidFile.json',
+  fileContent: [{}]
+}];
 
 describe('Inverted Index Suite:', () => {
-  // Parameters for isValid method tests
-  const validBook = InvertedIndex.isValid(validFile);
-  const invalidBook = InvertedIndex.isValid(invalidFile);
-  const malformedBook = InvertedIndex.isValid(malformedFile);
-  const emptyBook = InvertedIndex.isValid(emptyFile);
-
   describe('Inverted Index Method checker', () => {
+    it('should have \'validateFile\' as a method', () => {
+      expect(invObj.validateFile).toBeDefined('function');
+    });
+
     it('should have `isValid` as a method', () => {
       expect(InvertedIndex.isValid).toBeDefined('function');
     });
@@ -78,16 +66,37 @@ describe('Inverted Index Suite:', () => {
     });
   });
 
+  describe('Validate file function', () => {
+    it('should return a valid json', () => {
+      expect([{
+        'title': 'Queen Premier',
+        'text': 'Is he to go up? Yes, he is to go up.'
+      },
+      {
+        'title': 'Poem',
+        'text': 'Broken crayons still colour.'
+      }
+      ]).toEqual(fileContent);
+    });
+
+    it('should return \'Invalid JSON\' for Invalid files', () => {
+      expect(invalidContent).toEqual('Invalid Json');
+    });
+  });
+
   describe('Inverted index JSON Validation method', () => {
+    // Parameters for isValid method tests
+    const validBook = InvertedIndex.isValid(validFile);
+    const invalidBook = InvertedIndex.isValid(invalidFile);
+    const malformedBook = InvertedIndex.isValid(malformedFile);
+    const emptyBook = InvertedIndex.isValid(emptyFile);
+
     it('should return \'Valid JSON\' for Valid json types', () => {
-      // request
-      //   .post('/api/create')
-      //   .attach('validBook', 'fixtures/validFile.json')
       expect(validBook).toEqual('Valid JSON');
     });
 
     it('should return \'Invalid JSON\' for invalid JSON type', () => {
-      expect(invalidBook).toEqual('Invalid JSON');
+      expect(invalidBook).toEqual('Invalid Json');
     });
 
     it('should return \'Malformed JSON\' for malformed JSON type', () => {
@@ -101,15 +110,14 @@ describe('Inverted Index Suite:', () => {
 
   describe('Join text and title function', () => {
     it('should return a string with the joined text and title in lowercases', () => {
-      let newBook = [
-        {
-          'title': 'Alice in the wonder land',
-          'text': 'I am not as lazy as Alice'
-        },
-        {
-          'title': 'Lady',
-          'text': 'The lady is 50 years old. SHe has a child'
-        }
+      let newBook = [{
+        'title': 'Alice in the wonder land',
+        'text': 'I am not as lazy as Alice'
+      },
+      {
+        'title': 'Lady',
+        'text': 'The lady is 50 years old. SHe has a child'
+      }
       ];
 
       const newTextTitle = 'alice in the wonder land i am not as lazy as alice lady the lady is 50 years old. she has a child';
@@ -120,40 +128,40 @@ describe('Inverted Index Suite:', () => {
 
   describe('Remove duplicate words function', () => {
     it('should return only unique words in an array', () => {
-      let getWords =
-        ['broken',
-          'colour',
-          'crayons',
-          'go',
-          'go',
-          'he',
-          'he',
-          'is',
-          'is',
-          'poem',
-          'premier',
-          'queen',
-          'still',
-          'to',
-          'to',
-          'up',
-          'up',
-          'yes'];
+      let getWords = ['broken',
+        'colour',
+        'crayons',
+        'go',
+        'go',
+        'he',
+        'he',
+        'is',
+        'is',
+        'poem',
+        'premier',
+        'queen',
+        'still',
+        'to',
+        'to',
+        'up',
+        'up',
+        'yes'
+      ];
 
-      const uniqueWords =
-        ['broken',
-          'colour',
-          'crayons',
-          'go',
-          'he',
-          'is',
-          'poem',
-          'premier',
-          'queen',
-          'still',
-          'to',
-          'up',
-          'yes'];
+      const uniqueWords = ['broken',
+        'colour',
+        'crayons',
+        'go',
+        'he',
+        'is',
+        'poem',
+        'premier',
+        'queen',
+        'still',
+        'to',
+        'up',
+        'yes'
+      ];
 
       getWords = InvertedIndex.removeDuplicates(getWords);
       expect(uniqueWords).toEqual(getWords);
@@ -163,8 +171,7 @@ describe('Inverted Index Suite:', () => {
   describe('Tokenize function', () => {
     it('should return in an array tokens for each file', () => {
       // Parameter to be used to test for correctness of the tokenize method
-      let newBook = [
-        {
+      let newBook = [{
           'title': 'Alice in Wonderland',
           'text': 'I am not as lazy as Alice'
         },
@@ -175,50 +182,114 @@ describe('Inverted Index Suite:', () => {
       ];
 
       // Expected result after newBook is passed into tokenize function
-      const result =
-        ['50',
-          'a',
-          'alice',
-          'am',
-          'as',
-          'child',
-          'has',
-          'i',
-          'in',
-          'is',
-          'lady',
-          'lazy',
-          'not',
-          'old',
-          'she',
-          'the',
-          'wonderland',
-          'years'];
+      const result = ['50',
+        'a',
+        'alice',
+        'am',
+        'as',
+        'child',
+        'has',
+        'i',
+        'in',
+        'is',
+        'lady',
+        'lazy',
+        'not',
+        'old',
+        'she',
+        'the',
+        'wonderland',
+        'years'
+      ];
 
       newBook = InvertedIndex.tokenize(newBook);
       expect(result).toEqual(newBook);
     });
   });
 
-  describe('Create Index function', () => {
+  describe('Populate Index function', () => {
     it('should return error message if JSON file is `Invalid`, `Malformed` or `empty` file.', () => {
-      expect(invObj.createIndex(invalidFile)).toEqual('Error: The file is not a correct JSON file!');
+      expect(invObj.createIndex(invalidTestSpec)).toEqual('Invalid JSON');
+    });
+
+    it('should return valid createIndex obj', () => {
+      expect({
+        'validFile.json': {
+          go: [ 0 ],
+          he: [ 0 ],
+          is: [ 0 ],
+          premier: [ 0 ],
+          queen: [ 0 ],
+          to: [ 0 ],
+          up: [ 0 ],
+          yes: [ 0 ],
+          broken: [ 1 ],
+          colour: [ 1 ],
+          crayons: [ 1 ],
+          poem: [ 1 ],
+          still: [ 1 ]
+        }
+      }).toEqual(invObj.createIndex(validTestSpec));
     });
 
     it('should return in an object a file name as key, and accurate index for every word\'s  occurence in a book', () => {
-      const newFile = invObj.createIndex('validFile', validFile);
-      expect(createdIndex).toEqual(newFile);
+      // const newFile = invObj.createIndex('validFile', validFile);
+      // expect(createdIndex).toEqual(newFile);
+      expect({
+        'validFile.json': {
+          go: [ 0 ],
+          he: [ 0 ],
+          is: [ 0 ],
+          premier: [ 0 ],
+          queen: [ 0 ],
+          to: [ 0 ],
+          up: [ 0 ],
+          yes: [ 0 ],
+          broken: [ 1 ],
+          colour: [ 1 ],
+          crayons: [ 1 ],
+          poem: [ 1 ],
+          still: [ 1 ]
+        }
+      }).toEqual(invObj.createIndex(validTestSpec));
     });
   });
 
   describe('Search Index function', () => {
+    // Test Parameter for SearchIndex test
+    const createdIndex = {
+      validFile: {
+        go: [ 0 ],
+        he: [ 0 ],
+        is: [ 0 ],
+        premier: [ 0 ],
+        queen: [ 0 ],
+        to: [ 0 ],
+        up: [ 0 ],
+        yes: [ 0 ],
+        broken: [ 1 ],
+        colour: [ 1 ],
+        crayons: [ 1 ],
+        poem: [ 1 ],
+        still: [ 1 ]
+      }
+    };
+
+    const searchResult = {
+      'validFile.json': {
+        crayons: [ 1 ],
+        poem: [ 1 ],
+        colour: [ 1 ]
+      }
+    };
+
     it('should return the correct index for every search term', () => {
       const newSearch = invObj.searchIndex(createdIndex, 'validFile', 'crayons', ['poem'], 'colour', ['not']);
       expect(searchResult).toEqual(newSearch);
     });
 
     it('should return false if search not found in the file', () => {
-      const newSearch = invObj.searchIndex(createdIndex, 'validFile', 'air', 'bear', ['care']);
+      const newSearch = invObj.searchIndex(validTestSpec, 'validFile', 'air', 'bear', ['care']);
       expect(newSearch).toEqual(false);
     });
   });
